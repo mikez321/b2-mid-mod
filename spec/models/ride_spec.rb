39dd0eac828db5f2c15a2fabffd266427723f6a5
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-03 11:31:47 -0600

    Finish has many setup of mechanics and rides

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -4,5 +4,6 @@ describe Ride, type: :model do
   describe "relationships" do
     it {should belong_to :park}
     it {should have_many :mechanic_rides}
+    it {should have_many(:mechanics).through(:mechanic_rides)}
   end
 end
