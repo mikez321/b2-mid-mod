82463b9ff9493c37d23113ff16b0a22599975edb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-03 09:46:04 -0600

    expand test to show mechanics

--- spec/features/mechanic_index_spec.rb
+++ spec/features/mechanic_index_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe "When I visit a mechanics index page", type: :feature do
   it "I see a header saying All Mechanics and see mechanics names and years of experience" do
 
+    mechanic_1 = Mechanic.create!(name: "Sam Mills", experience: 10)
+    mechanic_2 = Mechanic.create!(name: "Kara Smith", experience: 11)
+
+
     visit "/mechanics"
 
     expect(page).to have_content("All Mechanics")
-
-    mechanic_1 = Mechanic.create!(name: "Sam Mills", experience: 10)
-    mechanic_1 = Mechanic.create!(name: "Kara Smith", experience: 11)
-
+    
     expect(page).to have_content("Sam Mills")
     expect(page).to have_content("10 years of experience")
 
