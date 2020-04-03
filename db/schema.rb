e27556833f6ca50c50453450936cbfdac00f9095
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-03 09:56:46 -0600

    Add rides and parks tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200403151146) do
+ActiveRecord::Schema.define(version: 20200403155557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 20200403151146) do
   create_table "mechanics", force: :cascade do |t|
     t.string "name"
     t.integer "experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parks", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.string "name"
+    t.integer "thrill_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
