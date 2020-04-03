203a6b5c4e040c3c4cd2e905ae7f25b37f3edb45
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-03 10:05:57 -0600

    Update show routes and create parks controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get "/mechanics", to: "mechanics#index"
 
+  get "/parks/:park_id", to:"parks#show"
+
 end
--- spec/features/parks/park_show_page_spec.rb
+++ spec/features/parks/park_show_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "when I visit a parks show page I see the name and price of admission", type: :feature do
   it "I also see the names of rides at the park in alphabetical order" do
 
-    park_1 = Park.create!(name: "Hershey Park", admission: 50)
+    park_1 = Park.create!(name: "Hershey Park", price: 50)
 
     visit "/parks/#{park_1.id}"
 
